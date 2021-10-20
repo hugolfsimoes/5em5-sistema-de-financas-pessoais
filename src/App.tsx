57b160c6a08b559bdca7718ac9de699b20d00336
hugolfsimoes <hugolfsimoes@gmail.com>
2021-10-20 12:06:08 -0300

    criado cabecalho, e body

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import * as C from './app.styles';
+import { Item } from './types/Item'
+import { Category } from './types/Category'
+import { items } from './data/items'
+import { categories } from './data/categories'
+import { getCurrentMonth, filterListByMonth } from './helpers/dateFilter'
 
 function App() {
+  const [ list, setList ] = useState(items);
+  const [ filteredList, setFilteredList ] = useState<Item[]>([])
+  const [ currentMonth, setCurrentMonth ] = useState(getCurrentMonth());
+
+  useEffect(() => {
+    setFilteredList(filterListByMonth(list, currentMonth))
+  }, [ list, currentMonth ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>Sistema Financeiro</C.HeaderText>
+      </C.Header>
+      <C.Body>
+        ...
+      </C.Body>
+    </C.Container>
   );
 }
 
